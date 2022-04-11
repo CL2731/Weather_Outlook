7b0684a62bcdc1b16ea13e4c3b2f6de2b680f824
Caitlin Lindauer <clilos018@gmail.com>
2022-04-11 09:14:04 -0600

    added last of code before debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,12 +102,84 @@ window.addEventListener("load", function () {
             localStorage.setItem("history", JSON.stringify(historyItems));
         }
     };
-    function searchWeather(){
+    function searchWeather(searchValue){
+        let endpoint= `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
+        fetch(endpoint)
+        .then((result)=> result.json())
+        .then((data)=> {
+            // Invoke our history method
+
+          if (!existingHistory.includes(searchValue)) {
+            handleHistory(searchValue);
+          }
+          // Clear any old content
+          todayEl = document.querySelector('#current-day');
+          todayEl.textContent = ' ';
+          // Create html content for current weather
+          let titleElement = document.createElement('h3');
+          titleElement.classList.add('card-title');
+          titleElement.textContent = `${
+            data.name
+          } (${new Date().toLocaleDateString()})`;
+
+          let cardElement = document.createElement('div');
+          cardElement.classList.add('card');
+
+          let windElement = document.createElement('p');
+          windElement.classList.add('card-text');
+
+          let humidityElement = document.createElement('p');
+          humidityElement.classList.add('card-text');
+          
+          let tempElement = document.createElement('p');
+          tempElement.classList.add('card-text');
+          humidityElement.textContent = `Humidity: ${data.main.humidity} %`;
+          tempElement.textContent = `Temperature: ${data.main.temp} °F`;
+
+          let cardBodyElement = document.createElement('div');
+               cardBodyElement.classList.add('card-body');
+
+          let imageElement = document.createElement('img');
+          imageElement.setAttribute(
+            'src',
+            `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
+          );
+  
+          titleElement.appendChild(imageElement);
+          cardBodyElement.appendChild(titleElement);
+          cardBodyElement.appendChild(tempElement);
+          cardBodyElement.appendChild(windElement);
+          cardBodyElement.appendChild(humidityElement);
+          cardElement.appendChild(cardBodyElement);
+          todayEl.appendChild(cardElement);
+          uvIndex(data.coord.latitude, data.coord.longitutde);
+          getWeather(searchValue);
+        })
 
     } 
 
     function newRow(searchValue){
         let liElement= document.createElement("li");
-        liElement.classList.add("list-group-item")
+        liElement.classList.add("list-group-item", "list-group-item-action");
+        liElement.id= searchValue;
+        let text= searchValue;
+        liElement.textContent= text;
+        liElement.addEventListener("click", (e)=> {
+            if (e.target.tagName === "LI"){
+                searchWeather(e.target.textContent);
+            }
+        })
+        document.getElementById("history").appendChild(liElement);
     }
+    if (existingHistory && existingHistory.length > 0) {
+        existingHistory.forEach((item)=> newRow(item));
+    }
+    function searchResults(){
+        let searchValue= document.querySelector("#search-value").value;
+        if (searchValue){
+            searchWeather(searchValue);
+            newRow(searchValue);
+            document.querySelector("#search-value").value= "";
+        }
+    } document.querySelector("#search-button").addEventListener("click", searchResults);
 })
